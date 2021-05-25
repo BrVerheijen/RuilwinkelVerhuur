30692e94ab384c00015bf113ae37d46a9c9c57a4
Nathan Groenveld <2062294groenveld@zuyd.nl>
2021-05-25 16:06:47 +0200

    Index page added (unused) code

--- RuilwinkelVerhuur/Views/Home/Index.cshtml.cs
+++ RuilwinkelVerhuur/Views/Home/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RuilwinkelVerhuur.Models.Classes;
 
 namespace RuilwinkelVerhuur.Views.Home
 {
@@ -11,6 +12,21 @@ namespace RuilwinkelVerhuur.Views.Home
     {
         public void OnGet()
         {
+            Product product = new Product(); 
+            product.ID = 1;
+            product.Name = "Stoel";
+            product.Picture = "https://www.horecaoutlet.nl/wp-content/uploads/2018/04/DSC_0062.jpg";
+            product.MaxHuurLengte = 14;
+            product.Availability = true;
+            product.Cost = 69;
+            product.Category = "Meubilair";
         }
+
+            
+                
+
+            
+
+        
     }
 }
