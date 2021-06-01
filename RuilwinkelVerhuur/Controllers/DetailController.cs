23e2fc78be40611ece99a0be0c65d149a005fce1
Bram Verheijen <2063119verheijen@zuyd.nl>
2021-06-01 11:53:14 +0200

    new branch

---
+++ RuilwinkelVerhuur/Controllers/DetailController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RuilwinkelVerhuur.Models.Classes;
+
+namespace RuilwinkelVerhuur.Controllers
+{
+    public class DetailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Product product)
+        {
+            int ProductID = product.ID;
+            string Name = product.Name;
+            bool Availability = product.Availability;
+            string Category = product.Category;
+            string Picture = product.Picture;
+            int Cost = product.Cost;
+            int MaxHuurLengte = product.MaxHuurLengte;
+
+
+            return View();
+        }
+    }
+}
