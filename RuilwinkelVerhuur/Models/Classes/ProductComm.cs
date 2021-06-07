66a5af776b77b8261f8a12202283749bf99b96e7
Nathan Groenveld <2062294groenveld@zuyd.nl>
2021-06-07 10:08:25 +0200

    Datastream full working external list + sending data through pages

--- RuilwinkelVerhuur/Models/Classes/ProductComm.cs
+++ RuilwinkelVerhuur/Models/Classes/ProductComm.cs
@@ -7,5 +7,29 @@ namespace RuilwinkelVerhuur.Models.Classes
 {
     public class ProductComm
     {
+
+        public static List<Product> retrieveList()
+        {
+            List<Product> publicProductList = new List<Product>() { };
+            publicProductList.Add(new Product { ID = 0, Name = "Stoel", Availability = true, Category = "Meubilair", Cost = 5, MaxHuurLengte = 14, Picture = "https://www.horecaoutlet.nl/wp-content/uploads/2018/04/DSC_0062.jpg" });
+            publicProductList.Add(new Product { ID = 1, Name = "iPad", Availability = false, Category = "Elektronika", Cost = 50, MaxHuurLengte = 14, Picture = "https://www.apple.com/" });
+
+            return publicProductList;
+        }
+
+        public static Product retrieveProducts(int searchid)
+        {
+            List<Product> productlist = ProductComm.retrieveList();
+            Product returnproduct = new Product();
+            foreach (var product in productlist)
+            {
+                if (product.ID == searchid)
+                {
+                    returnproduct = product;
+                }
+            }
+            return returnproduct;
+        }
     }
 }
+
