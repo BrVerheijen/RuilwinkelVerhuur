d5b4b673b5123eab4a5d7f4a1c05f3b0fa52caee
Peter Derks <plsderks@gmail.com>
2021-06-25 09:00:17 +0200

    Database opnieuw gekoppeld, cart is nu een 2d lijst, dropdown en datepicker werken nu(bijna)

--- RuilwinkelVerhuur/Controllers/DetailController.cs
+++ RuilwinkelVerhuur/Controllers/DetailController.cs
@@ -9,34 +9,35 @@ namespace RuilwinkelVerhuur.Controllers
 {
     public class DetailController : Controller
     {
-        //Sends productID to index page of details
+        
         public List<int> dropdownList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
-
+        //Sends productID to index page of details
         public ActionResult Index(int id)
         {            
             ViewBag.Message = id;
             ViewBag.Dropdown = dropdownList;
+
             return View();
         }
 
         //adds product to cart session if product is not in cart already
         public ActionResult AddToCart(int id)
         {
-            if (SessionHelper.GetObjectFromJson<List<int>>(HttpContext.Session, "cart") == null)
+            
+            if (SessionHelper.GetObjectFromJson<List<List<string>>>(HttpContext.Session, "cart") == null)
             {
-                List<int> cart = new List<int>();
-                cart.Add(id);
+                List<List<string>> cart = new List<List<string>>();                
                 SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
             }
             else
             {
                 bool isinlist = false;
 
-                List<int> cart = SessionHelper.GetObjectFromJson<List<int>>(HttpContext.Session, "cart");
+                List<List<string>> cart = SessionHelper.GetObjectFromJson<List<List<string>>>(HttpContext.Session, "cart");
                 
-                foreach (var i in cart)
+                foreach (List<string> i in cart)
                 {
-                    if (i == id)
+                    if (i[0] == id.ToString())
                     {
                         isinlist = true;
                     }
@@ -44,7 +45,11 @@ namespace RuilwinkelVerhuur.Controllers
                 
                 if (isinlist == false)
                 {
-                    cart.Add(id);
+                    List<string> itemInfo = new List<string>();
+                    itemInfo.Add(id.ToString());
+                    itemInfo.Add("30/06/2021");
+                    itemInfo.Add("1");
+                    cart.Add(itemInfo);
                 }
 
                 SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
